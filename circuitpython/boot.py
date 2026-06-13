cdc2a5180868d623622f343b5a3d3bfddd452be7
neatloaf <neatloaf7@gmail.com>
2026-06-12 21:17:46 -0700

    update code

--- circuitpython/boot.py
+++ circuitpython/boot.py
@@ -1,2 +1,3 @@
 import usb_cdc
-usb_cdc.enable(console=True, data=True)
+usb_cdc.enable(console=True, data=False)
+#WARNING: ESP32 S3 cannot support both serial lines open at once
--- circuitpython/code.py
+++ circuitpython/code.py
@@ -6,13 +6,14 @@ import fourwire
 import neopixel
 import adafruit_ili9341
 import time
-import adafruit_ft5336
-import adafruit_register
 import adafruit_focaltouch
+import terminalio
+from adafruit_display_text import label
+#from classes import MediaButton, Background
 
 displayio.release_displays()
-pixel = neopixel.NeoPixel(board.IO42, 10)
-pixel[0] = (255, 0, 0)
+pixel = neopixel.NeoPixel(board.IO42, 1)
+pixel[0] = (0, 255, 0)
 pixel.show()
 
 spi = busio.SPI(board.IO12, board.IO11)
@@ -23,9 +24,8 @@ display = adafruit_ili9341.ILI9341(display_bus, width=320, height=240)
 
 backlight = digitalio.DigitalInOut(board.IO45)
 backlight.direction = digitalio.Direction.OUTPUT
-print("hello nigga")
 backlight.value = True
-pixel.deinit()
+#pixel.deinit()
 
 i2c = busio.I2C(board.IO15, board.IO16)
 irq = digitalio.DigitalInOut(board.IO17)
@@ -34,13 +34,24 @@ irq.pull = digitalio.Pull.UP
 
 
 touch = adafruit_focaltouch.Adafruit_FocalTouch(i2c)
+text = "hello world"
+font = terminalio.FONT
+color = 0xFFFFFF
 
+text_area = label.Label(font, text=text, color=color)
+
+text_area.x = 100
+text_area.y = 100
+display.root_group = text_area
+
+#hello
 
 while True:
     t = touch.touches
-    print(t)
-    print(time.monotonic())
     if t:
-        print(f"x= {t[0]["x"]}")
+        x = t[0]["y"]
+        y = 239 - t[0]["x"]
+        text_area.x = x
+        text_area.y = y
+        print(f"x= {t[0]["y"]}, y= {t[0]["x"]}")
     pass
-
